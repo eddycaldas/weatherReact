df99ea4bbc54564a04562178fdaf48f0648db0fe
Eddy Caldas <eddycaldas@live.com>
2019-05-14 11:54:46 -0600

    build

---
+++ build/precache-manifest.4acb244de04ed6f9fb0e22074774af43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0956bb0ab8d2615b1443835e6e1875",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88e45ce18eb3f5840c2",
+    "url": "/static/css/main.44bf6841.chunk.css"
+  },
+  {
+    "revision": "f7d6e0430bef7b477bb2",
+    "url": "/static/js/2.5a7af95d.chunk.js"
+  },
+  {
+    "revision": "e88e45ce18eb3f5840c2",
+    "url": "/static/js/main.88d26327.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
